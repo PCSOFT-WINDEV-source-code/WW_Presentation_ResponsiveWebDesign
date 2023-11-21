0d78701b47ba18e5797d7604c0b389bb4d085a9f
PCSOFT-WINDEV <appli@pcsoft.fr>
2023-11-21 19:18:58 +0100

    Initial Commit

---
+++ exinfos.ex
@@ -0,0 +1,9 @@
+[INFOS]
+gaf=0
+title=WW_Presentation_ResponsiveWebDesign
+image=description.gif
+description=description.rtf
+source=0
+typesite=0
+[OBJECT]
+WW_Presentation_ResponsiveWebDesign.wwp=4096
